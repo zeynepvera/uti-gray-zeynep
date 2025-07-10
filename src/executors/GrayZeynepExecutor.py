b53178b29a799caae86fd5642d29e7ccfd3ae0b9
Zeynep Dağtekin <zeynep.dagtekin@stu.fsm.edu.tr>
2025-07-10 11:32:14 +0300

    feat: response.py, GrayZeynepExecutor.py fix: PackageModel.py

---
+++ src/executors/GrayZeynepExecutor.py
@@ -0,0 +1,52 @@
+"""
+    It is one of the preprocessing components in which the image is rotated.
+"""
+
+import os
+import cv2
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
+
+from sdks.novavision.src.media.image import Image
+from sdks.novavision.src.base.component import Component
+from sdks.novavision.src.helper.executor import Executor
+from components.GrayZeynep.src.utils.response import build_response
+from components.GrayZeynep.src.models.PackageModel import PackageModel
+
+
+class GrayZeynepExecutor(Component):
+    def __init__(self, request, bootstrap):
+        super().__init__(request, bootstrap)
+        self.request.model = PackageModel(**(self.request.data))
+        self.rotation_degree = self.request.get_param("Degree")
+        self.keep_side = self.request.get_param("KeepSide")
+        self.image = self.request.get_param("inputImage")
+
+
+    @staticmethod
+    def bootstrap(config: dict) -> dict:
+        return {}
+
+
+    def gray(self,img):
+
+        """
+        Convert image to grayscale.
+        """
+
+        return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
+
+    def run(self):
+        img = Image.get_frame(img=self.image, redis_db=self.redis_db)
+        img.value = self.gray(img.value)
+        self.image = Image.set_frame(img=img, package_uID=self.uID, redis_db=self.redis_db)
+        packageModel = build_response(context=self)
+        return packageModel
+
+
+if "__main__" == __name__:
+    Executor(sys.argv[1]).run()
--- src/utils/response.py
+++ src/utils/response.py
@@ -1,15 +1,15 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, GrayZeynepExecutorOutputs, GrayZeynepExecutorResponse, GrayZeynepExecutor, OutputImage
 
 
 def build_response(context):
     outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
-    packageConfigs = PackageConfigs(executor=executor)
+    grayZeynepExecutorOutputs= GrayZeynepExecutorOutputs(outputImage=outputImage)
+    grayZeynepExecutorResponse = GrayZeynepExecutorResponse(outputs=grayZeynepExecutorOutputs)
+    grayZeynepExecutor=GrayZeynepExecutor(value=grayZeynepExecutorResponse)
+    configexecutor = ConfigExecutor(value=grayZeynepExecutor)
+    packageConfigs = PackageConfigs(executor=configexecutor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
     return packageModel
