23cd918d66c917c1c4a702238a2277797a7b950b
Zeynep Dağtekin <zeynep.dagtekin@stu.fsm.edu.tr>
2025-07-11 15:08:04 +0300

    fix: GrayZeynepExecutor.py

--- src/executors/GrayZeynepExecutor.py
+++ src/executors/GrayZeynepExecutor.py
@@ -21,7 +21,7 @@ class GrayZeynepExecutor(Component):
         self.request.model = PackageModel(**(self.request.data))
         self.rotation_degree = self.request.get_param("Degree")
         self.keep_side = self.request.get_param("KeepSide")
-        self.image = self.request.get_param("inputImage")
+        self.image = self.request.get_param("inputImageOne")
 
 
     @staticmethod
